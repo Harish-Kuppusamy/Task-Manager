b29d79eacfb929b69abd4c291c389b1b52be3c68
Harish-Kuppusamy <ksharishcs20@gmail.com>
2025-05-16 16:25:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import Home from './pages/Home'
+import Create from './pages/Create'
+import Edit from './pages/Edit'
+import { Routes, Route, Link } from "react-router-dom";
+import { useState } from 'react';
+import { useEffect } from "react";
+
+import './App.css'
+
+function App() {
+
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+  return (
+    <>
+      <nav className=" w-full bg-blue-600 p-4 flex justify-center items-center gap-6 text-white font-semibold text-lg shadow-md ">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <span>|</span>
+        <Link to="/create" className="hover:underline">
+          Create New Task
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route
+          path="/"
+          element={<Home tasks={tasks} setTasks={setTasks} />}
+        ></Route>
+        <Route path="/create" element={<Create setTasks={setTasks} />}></Route>
+        <Route
+          path="/edit/:id"
+          element={<Edit tasks={tasks} setTasks={setTasks} />}
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App
